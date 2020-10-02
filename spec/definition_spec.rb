98d765a09a2c18be510a1b20405aa63a86971a99
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-02 10:01:43 -0700

    PASS test to initialize definition object

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -10,7 +10,7 @@ describe('#Definition') do
       definition = Definition.new({name: "the greatest of all time", id: nil, word_id: 2})
       expect(definition.name).to(eq("the greatest of all time"))
       expect(definition.id).to(eq(1))
-      expect(definition.word_id).to(eq(1))
+      expect(definition.word_id).to(eq(2))
     end
   end
 
