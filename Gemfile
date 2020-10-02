708d2086cd6d4035466cfbefbbb91696c53de0f3
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-02 08:09:07 -0700

    Add initial file structure and Gemfile bundle with starter code

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source('https://rubygems.org')
+
+gem('capybara')
+gem('pry')
+gem('rspec')
+gem('sinatra')      
+gem('sinatra-contrib')
---
+++ app.rb
@@ -0,0 +1,6 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/word')
+require('./lib/definition')
+require('pry')
+also_reload('lib/**/*.rb')
