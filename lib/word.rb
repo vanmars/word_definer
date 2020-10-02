d4fc2d9382dac7b577c28ed203f1bf49195946a2
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-02 09:04:50 -0700

    Write and FAIL test for Word.all method

--- lib/word.rb
+++ lib/word.rb
@@ -1,6 +1,5 @@
 class Word
-  attr_reader :id
-  attr_accessor :name 
+  attr_reader :id, :name
 
   @@words = {}
   @@total_rows = 0
@@ -9,5 +8,33 @@ class Word
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id) || @@total_rows += 1
   end
+
+  # Create Word
+  def ==(other_word)
+  end
+
+  def save
+  end
+
+  # Read Words
+  def self.all
+  end
+
+  def self.find
+  end
+ 
+  # Update Word
+  def update
+  end
+
+
+  # Delete Word
+  def delete
+  end
+
+  # For Testing
+  def self.clear
+  end
+
 
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -8,7 +8,12 @@ describe('#Word') do
       word = Word.new({name: "Vanessa", id:1})
       expect(word.id).to(eq(1))
     end
+  end
 
+  describe('.all') do
+    it('returns all of the created words') do 
+      expect(Word.all).to(eq([]))
+    end
   end
 
 end
