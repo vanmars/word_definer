d065befb25673d0556dea49bff125beb820886a6
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-02 09:26:15 -0700

    Write and FAIL test for word == method

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -41,16 +41,14 @@ describe('#Word') do
     end
   end
 
-  
-
-  # describe('#==') do
-  #   it("is the same word if it has the same attributes as another word") do
-  #     word1 = Word.new({name: "hello", id: nil})
-  #     word1.save
-  #     word2 = Word.new({name: "there", id: nil})
-  #     word2.save
-  #     expect(word1).to(eq(word2))
-  #   end
-  # end
+  describe('#==') do
+    it("is the same word if it has the same attributes as another word") do
+      word1 = Word.new({name: "hello", id: nil})
+      word1.save
+      word2 = Word.new({name: "there", id: nil})
+      word2.save
+      expect(word1).to(eq(word2))
+    end
+  end
 
 end
