7be362f6b7601fe50c6d330a30d374c0efe68f6d
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-02 08:54:06 -0700

    PASS test to initialize a word object

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -6,7 +6,7 @@ describe('#Word') do
   describe('#id') do
     it('returns the id of an initialized word object') do
       word = Word.new({name: "Vanessa", id:1})
-      expect(word.id).to(eq(2))
+      expect(word.id).to(eq(1))
     end
 
   end
